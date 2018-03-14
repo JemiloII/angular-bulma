8c2027359854a882ddb2a938cae19dc8491cb66e
jemiloii <jemiloii@gmail.com>
2018-03-14 11:28:21 -0500

    Init

---
+++ src/app/modules/breadcrumbs/breadcrumbs.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BreadcrumbsComponent } from './breadcrumbs.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BreadcrumbsComponent],
+  exports: [BreadcrumbsComponent]
+})
+export class BreadcrumbsModule { }
